c3f0c95b457457ec87eaeb7d3d5c3291d2c5c8b9
Maxime St-Onge Amoretti <maximus@Maximes-MacBook-Pro.local>
2022-05-07 15:35:37 -0400

    Completed number 4

---
+++ Exercises/Closures_PrivateData_IIFEs/number_4.js
@@ -0,0 +1,22 @@
+function delegate(referenceObject, methodName, ...args) {
+  return function() {
+    return referenceObject[methodName].apply(referenceObject, args);
+  };
+}
+
+let foo = {
+  name: 'test',
+  bar: function(greeting) {
+    console.log(greeting + ' ' + this.name);
+  },
+};
+
+let baz = {
+  qux: delegate(foo, 'bar', 'hello'),
+};
+
+baz.qux();   // logs 'hello test';
+
+foo.bar = function() { console.log('changed'); };
+
+baz.qux();          // logs 'changed'
